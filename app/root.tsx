e6e5e5c877a52c40463722fd56cf97cf2ba556f7
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-05-27 21:18:12 +0200

    Add preconnects

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,8 @@ export const links: LinksFunction = () => {
      * <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
      * <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap" rel="stylesheet"></link>
      */
-
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true },
     {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap",
